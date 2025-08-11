e805ef4f6e101866a9c040997377beac17506e61
Magnus Ternström <magnus@falle.se>
2025-08-11 12:53:08 +0200

    Remove duplicate EchoUseCase implementation

--- core/domain/src/main/java/com/magter/myassistant/domain/EchoUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.magter.myassistant.domain
-
-/**
- * Simple use case that echoes back the provided input.
- */
-class EchoUseCase {
-    operator fun invoke(input: String): String = input
-}
